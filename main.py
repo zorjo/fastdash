8112db182de3db91797a592a9359ed82c0a3dd71
Sourjyo Deb <58564906+zorjo@users.noreply.github.com>
2024-07-01 14:29:23 +0530

    Updated

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-#from fastapi import FastAPI
-#from pydantic import BaseModel
-
-
-
-
 from fastapi_sso.sso.google import GoogleSSO
-from fastapi import FastAPI, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI, Depends,Security,Request
+from fastapi.security import OAuth2PasswordBearer,APIKeyCookie
 #from fastapi.security.utils import get_authorization_scheme_param
 #from fastapi.responses import RedirectResponse
 from fastapi.requests import Request
@@ -15,9 +9,17 @@ from pydantic import BaseModel
 #from google.oauth2 import id_token
 #from google.auth.transport import requests
 #import os
-import json
-import aiohttp
 
+import json
+
+def read_json_file(file_path: str) -> dict:
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+client=read_json_file("client.json")
+SECRET_KEY="9ce9230d35ee14142be93578ca79a27030d0b2ad87b6aa3abbeabb535ea38df6"
+GOOGLE_CLIENT_ID = client["web"]["client_id"]
+GOOGLE_CLIENT_SECRET = client["web"]["client_secret"]
 app = FastAPI()
 
 class Item(BaseModel):
@@ -37,19 +39,8 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 async def login():
     return {"message": "Login page"}
 
-import json
-
-def read_json_file(file_path: str) -> dict:
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-    return data
-client=read_json_file("client.json")
 
 # Google OAuth2 configuration
-from dotenv import load_dotenv
-load_dotenv()
-GOOGLE_CLIENT_ID = client["web"]["client_id"]
-GOOGLE_CLIENT_SECRET = client["web"]["client_secret"]
 google_sso = GoogleSSO(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, "http://localhost:5000/login/google/redirect/")
 
 
@@ -77,31 +68,3 @@ async def google_redirect(request: Request):
     return google_sso.access_token
     #token_url, state = await get_token(code)
     #return RedirectResponse(f"http://localhost:5000/homepage?token={token_url}")
-""""
-async def get_token(code: str):
-    token_url, error = await get_token_url(code)
-    if error:
-        return None, error
-    id_token = await get_id_token(token_url)
-    return token_url, None
-
-async def get_token_url(code: str):
-    headers = {"Content-Type": "application/x-www-form-urlencoded"}
-    data = {"grant_type": "authorization_code", "code": code, "redirect_uri": "http://localhost:5000/login/google/redirect", "client_id": GOOGLE_CLIENT_ID, "client_secret": GOOGLE_CLIENT_SECRET}
-    async with aiohttp.ClientSession() as session:
-        async with session.post("https://oauth2.googleapis.com/token", headers=headers, data=data) as response:
-            response_json = await response.json()
-            if "error" in response_json:
-                return None, response_json["error"]
-            return response_json["access_token"], None
-
-async def get_id_token(token_url: str):
-    headers = {"Authorization": f"Bearer {token_url}"}
-    async with aiohttp.ClientSession() as session:
-        async with session.get("https://openidconnect.googleapis.com/v1/userinfo", headers=headers) as response:
-            response_json = await response.json()
-            return response_json["email"]
-"""
-import uvicorn
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8002)
